a26bb4952eba0d8b3f330b1f3e2621f5a7714f0a
Kody <157696416+kydoCode@users.noreply.github.com>
2024-02-09 18:50:10 +0100

    Add exercices at day's end:
    Pushing exercice done
    Some more to come: exercice19 (class exercice) to be completed, adding isalpha if statement to exercice016, 'jeu du pendu' for challenged to be continued.

---
+++ exercice19.py
@@ -0,0 +1,26 @@
+class Student:
+    def __init__(self, name, age, mark):
+        self.name = name
+        self.age = age
+        self.mark = mark()
+    
+    def add_mark(self, one_mark):
+        self.mark.append[one_mark]
+    
+    def compute_average(self):
+        average = 0
+        average = sum(self.mark) / len(self.mark)
+        return average
+    
+    def display_facts(self):
+        print(f"Nom: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Moyenne: {self.compute_average()}")
+
+# Instances
+
+studentOne = Student("Eric", 20, [15, 17, 18]) 
+studentTwo = Student("Paul", 17, 17)
+studentThree = Student("Julia", 23, 14)
+
+print(studentOne.display_facts(), studentTwo.display_facts(), studentThree.display_facts())
