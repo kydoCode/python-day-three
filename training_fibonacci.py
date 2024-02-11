f2a55c8bc4eff9252a3d151169bd844173fc7348
Kody <157696416+kydoCode@users.noreply.github.com>
2024-02-11 18:38:22 +0100

    Add week end practice:
    Class Student (EX19) / Refactoring Fibonacci suite and adding it in func and class

---
+++ training_fibonacci.py
@@ -0,0 +1,18 @@
+# Formule de Fibonacci: 
+# F(n)=F(n−1)+F(n−2)
+## Calcul d'une valeur donnée a à un index n donné:
+# a(n+1) = a(n) + a(n-1)
+a = 0
+b = 1
+c: int
+d = [a, b]
+
+stopValue = int(input("Valeur d'arrêt pour la suite de Fibonacci : "))
+
+for element in range(d[0], stopValue):
+    print(d[element]) # affiche la liste à l'index de l'élément passé dans la boucle
+    c = b + a
+    d.append(c)
+    a = b
+    b = c
+print(d) # print(d) --> affiche toute la liste
